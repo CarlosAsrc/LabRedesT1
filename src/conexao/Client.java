8b1fc8c08a668fd9ebffa9241f231cecadaf7546
Christian2019 <37154877+Christian2019@users.noreply.github.com>
2019-10-03 16:25:11 -0300

    Update Client.java

--- src/conexao/Client.java
+++ src/conexao/Client.java
@@ -18,20 +18,22 @@ public class Client {
     
     public static void main(String[] args) throws SocketException, IOException, InterruptedException {
         
+    	System.out.println("Procurando Server....");
+    	
          BufferedReader clientRead =new BufferedReader(new InputStreamReader(System.in));
       
       InetAddress IP = InetAddress.getByName("192.168.0.18");
     
       DatagramSocket clientSocket = new DatagramSocket();
-      while(true)    //true
+      while(true)   
       {
     	 
       byte[] sendbuffer = new byte[1024];
       byte[] receivebuffer = new byte[1024];
       
-      System.out.print("\nClient: ");
-   //   String clientData = clientRead.readLine();
-      String clientData = "Clliente 2";
+      
+   
+      String clientData = "Cliente espera";
     
       if(clientData.equalsIgnoreCase("bye"))
       {
@@ -50,8 +52,7 @@ public class Client {
       String serverData = new String(receivePacket.getData());
       System.out.print("\nServer: " + serverData);
       
-      //checking condition for equals with serverData which is my string
-      //c--;
+     
       }
       clientSocket.close();
     }
