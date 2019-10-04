98a3108b2b005b49f72ba8af2266015bd2046ce8
Christian2019 <37154877+Christian2019@users.noreply.github.com>
2019-10-04 20:35:16 -0300

    j8

--- src/conexao/Acoes.java
+++ src/conexao/Acoes.java
@@ -41,6 +41,8 @@ public class Acoes {
 				y = array2[5];
 				local.clear();
 				local.add(Integer.parseInt(x));
+				String[] array3 = y.split(",");
+				y=array3[0];
 				local.add(Integer.parseInt(y));
 				Porta porta = new Porta(cor,chave,iten,situacao,local);
 				Jogo.portas.add(porta);
