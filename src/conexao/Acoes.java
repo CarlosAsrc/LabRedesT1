99ab607e8741417cec75789a56a7f61a4d35b6e8
Christian2019 <37154877+Christian2019@users.noreply.github.com>
2019-10-04 20:29:51 -0300

    j7

--- src/conexao/Acoes.java
+++ src/conexao/Acoes.java
@@ -1,36 +1,65 @@
 package conexao;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
+import regras.Jogo;
+import regras.Porta;
 
 public class Acoes {
-public static void lerAcao(Tarefa a) {
-	
-	String acao = a.acao;
-	String mensagem = a.mensagem;
-	
-	if (acao.equals("J1 Pronto!")) {
-		Estados.jogador1Pronto="Sim";
-	}
-    if (acao.equals("J2 Pronto!")) {
-    	Estados.jogador2Pronto="Sim";
-	}
-    if (acao.equals("iniciar")) {
-    	Estados.inicio="comece";
-	}
-    if (acao.equals("portas")) {
-    	System.out.println("bolacha");
-    	System.out.println(mensagem);
-	}
-	
-}
+	public static void lerAcao(Tarefa a) {
 
-public static void criarAcao(String a, String b) throws IOException {
-	Persistencia.readenviar();
-	String acao = a;
-    String mensagem =  b;
-    Tarefa tarefa = new Tarefa(acao,mensagem);
-    GerenciadorTarefa.envio.add(tarefa);
-    Persistencia.save("enviar");
+		String acao = a.acao;
+		String mensagem = a.mensagem;
+
+		if (acao.equals("J1 Pronto!")) {
+			Estados.jogador1Pronto = "Sim";
+		}
+		if (acao.equals("J2 Pronto!")) {
+			Estados.jogador2Pronto = "Sim";
+		}
+		if (acao.equals("iniciar")) {
+			Estados.inicio = "comece";
+		}
+		if (acao.equals("portas")) {
+			System.out.println("bolacha");
+			String cor;
+			String chave;
+			String iten;
+			String situacao;
+			String x;
+			String y;
+			ArrayList<Integer> local = new ArrayList<Integer>();
+			String[] array1 = mensagem.split(">");
+			for (int i = 0; i < array1.length; i++) {
+				String[] array2 = array1[i].split(":");
+				cor = array2[0];
+				chave = array2[1];
+				iten = array2[2];
+				situacao = array2[3];
+				x = array2[4];
+				y = array2[5];
+				local.clear();
+				local.add(Integer.parseInt(x));
+				local.add(Integer.parseInt(y));
+				Porta porta = new Porta(cor,chave,iten,situacao,local);
+				Jogo.portas.add(porta);
+			}
+for (int i=0;i<Jogo.portas.size();i++) {
+	System.out.println(Jogo.portas.get(i));
 }
+			
+		}
+
+	}
+
+	public static void criarAcao(String a, String b) throws IOException {
+		Persistencia.readenviar();
+		String acao = a;
+		String mensagem = b;
+		Tarefa tarefa = new Tarefa(acao, mensagem);
+		GerenciadorTarefa.envio.add(tarefa);
+		Persistencia.save("enviar");
+	}
 
 }
