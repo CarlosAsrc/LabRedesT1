a0a5a39170071d0a8d162fd38beb98bcc73da243
Christian2019 <37154877+Christian2019@users.noreply.github.com>
2019-10-06 23:41:37 -0300

    invi

--- src/regras/IA.java
+++ src/regras/IA.java
@@ -58,6 +58,7 @@ public class IA {
 
 		// Ambos invisíveis
 		if ((!visibilidade1) && (!visibilidade2)) {
+			System.out.println("Entrou na situacao 2 invi");
 			movimentoaleatoriovalido(xia,yia);
 		}
 
@@ -74,8 +75,10 @@ public class IA {
 
 		// Somente um visível
 		if (visibilidade1) {
+			System.out.println("Entrou na situacao de apenas 1 invi e foi o v1 invi");
 			return	movimentofinal(xia,yia,definirDirecao(x1, y1));
 		} else {
+			System.out.println("Entrou na situacao de apenas 1 invi e foi o v2 invi");
 			return	movimentofinal(xia,yia,definirDirecao(x2, y2));
 		}
 	}
